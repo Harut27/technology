244adf1e9a15f4ca5cd14f4ebebaded39970bea1
Harut Aghayan <agayanharut1@gmail.com>
2019-12-05 20:13:54 +0400

    footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import "./style.scss"
-
+import { Link } from "react-router-dom";
+import "./style.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { famail,faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faPhone,faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 
 const Footer = props => {
 
     return (
             <footer>
-                <div className="left-section">
+                <div className="footer-page">
+
+                    <span className="footer-text"> We Suggest The Best News </span>
+
+                    <Link to={'/news'}>NEWS</Link>
 
                 </div>
-                <div className="right-section">
+                <div className="footer-contact">
+                   
+                    <div><FontAwesomeIcon icon={faEnvelope}/>Email : gstech@gmail.com</div>
+                    <div><FontAwesomeIcon icon={faPhone}/>Phone : +374-00-00-00</div>
 
                 </div>
             </footer>
