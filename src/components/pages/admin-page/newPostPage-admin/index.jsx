0a1f977b5e82cd41fbdafe43bd728393fc46b6bf
Harut Aghayan <agayanharut1@gmail.com>
2020-02-18 19:21:37 +0400

    dfffsda

--- src/components/pages/admin-page/newPostPage-admin/index.jsx
+++ src/components/pages/admin-page/newPostPage-admin/index.jsx
@@ -1,72 +1,40 @@
-import React, {  useState } from 'react';
+import React, {  Component } from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ReactHtmlParser from "react-html-parser";
 import "./style.scss";
 
-const NewPostForAdmin = ()=>{
+class NewPostForAdmin extends Component {
 
 
+    handleOnChange = ()=>{
 
-    const [titleValue, setTitleValue] = useState('')
-
-    const handleOnChange = (e , editor)=>{
-
-        const data = editor.getData()
-        setTitleValue(data)
-        
     }
 
+    handleInputChange = ()=>{
+
+    }
+    
+    render(){
 
         return (
                 <div className="admin-new-post flex-column">
       
                     <div className="flex-column">
-                        
-                        <div className="flex-column">
-                            <span className="spanTitle">Please write your TITLE</span>
-                            <h2>
-                                <CKEditor
-                                    editor={ ClassicEditor }
-                                    onChange={handleOnChange}
-                                />
-                            </h2>
-                        </div>
 
-                        <div>
-                            {ReactHtmlParser(titleValue)}
-                        </div>
-        
-                        <div className="flex-column">
-                            <span className="spanTitle">Please write your CATEGORY</span>
-                            <span>
-                                <CKEditor
-                                    editor={ ClassicEditor }
-                                />
-                            </span>
-                        </div>
-        
-                        <div className="flex-column">
-                            <span className="spanTitle">Please write your SUBTITLE</span>
-                            <span>
-                                <CKEditor
-                                    editor={ ClassicEditor }
-                                />
-                            </span>
-                        </div>
-      
-                    </div>
-      
-                    <img src={`#`} alt=""/>
-      
-                    <div className="">
-                      {/* <div dangerouslySetInnerHTML={{ __html:post.content}} /> */}
+                        <form >
+                            <input id="input-title" type="text" placeholder="Please Write Title"/>
+                        </form>
+                     
+
                     </div>
       
                 </div>
-               
           
         );
+    }
 }
+
+
 
 export default NewPostForAdmin;
